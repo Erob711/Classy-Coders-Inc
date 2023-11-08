bd888d3bf15bbfeb35991f752e85c22a9d358476
Eric Roberson <109642366+Erob711@users.noreply.github.com>
2023-11-08 13:41:53 -0700

    employees and manager classes (#1)

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,36 @@
 class Employees {
+  //variables
+  #salary;
+  #isHired = true;
 
+  constructor(name, position, salary) {
+    this.name = name;
+    this.position = position;
+    this.#salary = salary;
+    this.#isHired;
+  }
 
+  getSalary() {
+    return this.#salary;
+  }
 
-    
+  setSalary(amount) {
+    this.#salary = amount;
+  }
+
+  getStatus() {
+    return this.#isHired;
+  }
+
+  setStatus(command) {
+    if (command == "hire") {
+      this.#isHired = true;
+    } else if (command == "fire") {
+      this.#isHired = false;
+    }
+  }
 }
 
 module.exports = {
-    Employees,
-}
+  Employees,
+};
